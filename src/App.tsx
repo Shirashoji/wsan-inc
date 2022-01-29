14f175b336b00a8c636a0d0bd6c86c4d8195fee6
Takuma SHIRASHOJI <tkm.shira@gmail.com>
2022-01-29 15:33:24 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ function App() {
       <Navigator>
         <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
-            <Route path="" element={<MainPage />} />
-            <Route path="PageA" element={<PageA />} />
-            <Route path="PageB" element={<PageB />} />;
-            <Route path="Images" element={<ImageMasonry />} />;
-            <Route path="members/*" element={<Members />} />;
+            <Route path="/" element={<MainPage />} />
+            <Route path="/PageA" element={<PageA />} />
+            <Route path="/PageB" element={<PageB />} />;
+            <Route path="/Images" element={<ImageMasonry />} />;
+            <Route path="/members/*" element={<Members />} />;
           </Routes>
         </BrowserRouter>
       </Navigator>
